d2423145779c0a9c92ceb98116ed7243968459db
mishranilesh012 <mishranilesh012@gmail.com>
2019-04-02 23:41:49 -0700

    Connect Update

--- Angular/NLP/src/app/app.component.ts
+++ Angular/NLP/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NLP';
+  base64textString:any;
+  imageData:any;
+
+  getString(val){
+    return val;
+  }
+
+  selectFile(event){
+    var files = event.target.files;
+    var file = files[0];
+
+  if (files && file) {
+      var reader = new FileReader();
+
+      reader.onload =this.handleFile.bind(this);
+
+      reader.readAsBinaryString(file);
+  }
+}
+
+handleFile(event) {
+  var binaryString = event.target.result;
+         this.base64textString= btoa(binaryString);
+         console.log(btoa(binaryString));
+         this.imageData = btoa(binaryString);
+
+         this.getString(this.imageData)
+ }
+
+
 }
--- Python/OCR.py
+++ Python/OCR.py
@@ -1,28 +1,39 @@
 import cv2
 import pytesseract
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+import base64
+import json
+app = Flask(__name__)
+api = Api(app)
 
-if __name__ == '__main__':
+def convertBaseToImage(imgstring):
+    imgdata = base64.b64decode(imgstring)
+    filename = 'some_image.jpg'  # I assume you have a way of picking unique filenames
+    with open(filename, 'wb') as f:
+        f.write(imgdata)
 
-    # if len(sys.argv) < 2:
-    #     print('Usage: python ocr_simple.py image.jpg')
-    #     sys.exit(1)
+class CheckOCR():
+    def post(self):
+        req_data = json.get_json()
+        image = req_data['imageData']
+        convertBaseToImage(image)
 
-    # Read image path from command line
-    imPath = 'test3.JPG'
+        imPath = 'some_image.JPG'
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        config = ('-l eng --oem 1 --psm 3')
+        # Read image from disk
+        im = cv2.imread(imPath, cv2.IMREAD_COLOR)
 
-    # Uncomment the line below to provide path to tesseract manually
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        # Run tesseract OCR on image
+        text = pytesseract.image_to_string(im, config=config)
 
-    # Define config parameters.
-    # '-l eng'  for using the English language
-    # '--oem 1' for using LSTM OCR Engine
-    config = ('-l eng --oem 1 --psm 3')
+        # Print recognized text
+        return jsonify(
+            Text = text
+        )
 
-    # Read image from disk
-    im = cv2.imread(imPath, cv2.IMREAD_COLOR)
-
-    # Run tesseract OCR on image
-    text = pytesseract.image_to_string(im, config=config)
-
-    # Print recognized text
-    print(text)
+# if __name__ == '__main__':
+#
+api.add_resource(CheckOCR,"/checkOcr")
+app.run(port = 5000, debug = True)
